8c161a031ada17618cb7a00c69c0a41527fa5664
TyyyCardoso <86069519+TyyyCardoso@users.noreply.github.com>
2022-05-19 19:44:42 +0100

    Randooommmmm

---
+++ electionsPOO/src/electionspoo/multimedia/ele.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package electionspoo.multimedia;
+
+/**
+ *
+ * @author User
+ */
+public class ele {
+    
+}
